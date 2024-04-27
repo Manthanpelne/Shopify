f46498d276f6481d667e4f4444ed8ae76e899c74
Manthanpelne <111408525+Manthanpelne@users.noreply.github.com>
2024-04-28 02:11:31 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -69,6 +69,16 @@ app.post('/webhook', express.raw({type: 'application/json'}), (request, response
 
 
 //middlewares
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+
 app.use(express.json());
 app.use(cookies());
 app.use(express.static("dist"));
