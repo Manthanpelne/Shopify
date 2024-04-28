9508e707afbdc4ee9777cb4b1f2fd8c573232355
Manthanpelne <111408525+Manthanpelne@users.noreply.github.com>
2024-04-28 16:43:22 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy:"https://shopify-eight-steel.vercel.app",
+    origin:"https://shopify-client-seven.vercel.app"
+  },
   plugins: [react()],
 })
