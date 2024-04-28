5eb69a5c32dc0e6b6c6aa55aff1ba1c2c7e076b8
Manthanpelne <111408525+Manthanpelne@users.noreply.github.com>
2024-04-28 18:22:53 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -73,13 +73,18 @@ app.post('/webhook', express.raw({type: 'application/json'}), (request, response
 
 app.use(express.json());
 app.use(cookies());
-app.use(express.static("dist"));
+//app.use(express.static("dist"));
+const corsOption = {
+  origin:"*",
+  optionsSucessStatus:"200"
+}
 app.use(
-  cors({
+  cors(corsOption,{
     exposedHeaders: ["X-Total-Count"],
-    origin:"https://shopify-client-seven.vercel.app"
-  })
+  }),
 );
+
+
 app.use(
   session({
     secret: process.env.session_secret,
