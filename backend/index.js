bc9c4e283969d06ed55908c4a5a891045245e79c
Manthanpelne <111408525+Manthanpelne@users.noreply.github.com>
2024-05-07 17:57:51 +0530

    header for cors

--- backend/index.js
+++ backend/index.js
@@ -81,6 +81,16 @@ app.use(
     origin:"*"
   })
 );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+
 app.use(
   session({
     secret: process.env.session_secret,
