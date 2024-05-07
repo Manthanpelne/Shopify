56530cf71182923e51eadc5a7545fb654386bb33
Manthanpelne <111408525+Manthanpelne@users.noreply.github.com>
2024-05-07 08:47:32 +0530

    updated code

--- frontend/src/pages/stripeCheckout.jsx
+++ frontend/src/pages/stripeCheckout.jsx
@@ -18,7 +18,7 @@ export default function StripeCheckout() {
 
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    fetch("http://localhost:8080/create-payment-intent", {
+    fetch("https://rich-cyan-octopus-fez.cyclic.app/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({totalAmount:currentOrder.totalAmount, orderId:currentOrder.id})
