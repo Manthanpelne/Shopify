624a6b6f55cae181bf9605b4a6874967396f50f9
Manthanpelne <111408525+Manthanpelne@users.noreply.github.com>
2024-05-07 13:20:42 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy:"https://shopify-gt8x.vercel.app"
+    proxy:"https://glittery-pastelito-6d7bd8.netlify.app"
   },
   plugins: [react()],
 })
