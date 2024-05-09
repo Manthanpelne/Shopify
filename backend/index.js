ca6c2b98935abebb7fb486298ebf575dfbc5831d
Manthanpelne <111408525+Manthanpelne@users.noreply.github.com>
2024-05-09 17:20:31 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -77,13 +77,13 @@ app.use(express.static("dist"));
 app.use(
   cors({
     exposedHeaders: ["X-Total-Count"],
-    origin:"https://lighthearted-paprenjak-ada158.netlify.app",
+    origin:"*",
     credentials:true,
     preflightContinue: true
   })
 );
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://lighthearted-paprenjak-ada158.netlify.app");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, PATCH, OPTIONS"),
   res.header(
     "Access-Control-Allow-Headers",
